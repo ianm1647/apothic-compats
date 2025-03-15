3f08831149a0f753ae9491876e7e52848cc50bf0
ianm1647 <ianmcg1128@gmail.com>
2025-03-15 16:09:50 -0500

    fixes #1 and updates malum gem textures thank you sammy;

--- src/main/java/ianm1647/apothic_compats/loot/ModLootCategories.java
+++ src/main/java/ianm1647/apothic_compats/loot/ModLootCategories.java
@@ -5,9 +5,10 @@ import com.sammy.malum.common.item.curiosities.weapons.scythe.MalumScytheItem;
 import com.sammy.malum.common.item.curiosities.weapons.staff.AbstractStaffItem;
 import dev.shadowsoffire.apotheosis.Apoth;
 import dev.shadowsoffire.apotheosis.loot.LootCategory;
+import dev.shadowsoffire.apothic_attributes.api.ALObjects;
+import dev.shadowsoffire.apothic_attributes.modifiers.EntitySlotGroup;
 import dev.shadowsoffire.placebo.registry.DeferredHelper;
 import ianm1647.apothic_compats.ApothicCompats;
-import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModList;
@@ -25,13 +26,13 @@ public class ModLootCategories {
         bus.register(R);
         if (ModList.get().isLoaded("aether")) {
             DART_SHOOTER = register("dart_shooter",
-                    s -> s.getItem() instanceof DartShooterItem, EquipmentSlotGroup.MAINHAND);
+                    s -> s.getItem() instanceof DartShooterItem, ALObjects.EquipmentSlotGroups.MAINHAND);
         }
         if (ModList.get().isLoaded("malum")) {
             SCYTHE = register("scythe",
-                    s -> s.getItem() instanceof MalumScytheItem, EquipmentSlotGroup.MAINHAND);
+                    s -> s.getItem() instanceof MalumScytheItem, ALObjects.EquipmentSlotGroups.MAINHAND);
             STAFF = register("staff",
-                    s -> s.getItem() instanceof AbstractStaffItem, EquipmentSlotGroup.MAINHAND);
+                    s -> s.getItem() instanceof AbstractStaffItem, ALObjects.EquipmentSlotGroups.MAINHAND);
         }
     }
 
@@ -47,11 +48,11 @@ public class ModLootCategories {
         return cat == STAFF;
     }
 
-    private static LootCategory register(String path, Predicate<ItemStack> filter, EquipmentSlotGroup slots, int priority) {
+    private static LootCategory register(String path, Predicate<ItemStack> filter, EntitySlotGroup slots, int priority) {
         return R.custom(path, Apoth.BuiltInRegs.LOOT_CATEGORY.key(), new LootCategory(filter, slots, priority));
     }
 
-    private static LootCategory register(String path, Predicate<ItemStack> filter, EquipmentSlotGroup slots) {
+    private static LootCategory register(String path, Predicate<ItemStack> filter, EntitySlotGroup slots) {
         return register(path, filter, slots, 1000);
     }
 
