0eb414452addaa0aa9635bfee1a65823fc4be7b7
ianmc <ianmcg1128@gmail.com>
2026-09-15 21:41:41 -0500

    try infinite ammo affix

--- src/main/java/ianm1647/apothic_compats/event/AffixEvents.java
+++ src/main/java/ianm1647/apothic_compats/event/AffixEvents.java
@@ -1,15 +1,15 @@
 package ianm1647.apothic_compats.event;
 
-import net.neoforged.neoforge.event.entity.EntityInvulnerabilityCheckEvent;
+import ianm1647.apothic_compats.affix.irons_artifice.InfiniteAmmoAffix;
+import io.redspace.irons_artifice.api.AmmoEvent;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
 
 public class AffixEvents {
 
-    public void modifyIncomingDamageTags(EntityInvulnerabilityCheckEvent e) {
-//        if (ModList.get().isLoaded("aether")) {
-//            MagicalDartAffix.modifyIncomingDamageTags(e);
-//        }
-//        if (ModList.get().isLoaded("create")) {
-//            MagicalPotatoAffix.modifyIncomingDamageTags(e);
-//        }
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void preventAmmoConsumption(AmmoEvent.Consume event) {
+        InfiniteAmmoAffix.preventAmmoConsumption(event);
     }
+
 }
